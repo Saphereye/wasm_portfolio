11c7ee770865bd4474c814a8428b267bca36a201
Saphereye <adarshdas950@gmail.com>
2023-11-09 17:24:08 +0530

    feat: Added sub windows for About, Resume and Projects

--- src/app.rs
+++ src/app.rs
@@ -1,25 +1,35 @@
+#[derive(serde::Deserialize, serde::Serialize)]
+enum Window {
+    About,
+    Resume,
+    Projects,
+}
+
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
-pub struct TemplateApp {
+pub struct Website {
     // Example stuff:
     label: String,
 
     #[serde(skip)] // This how you opt-out of serialization of a field
     value: f32,
+
+    current_window: Window,
 }
 
-impl Default for TemplateApp {
+impl Default for Website {
     fn default() -> Self {
         Self {
             // Example stuff:
             label: "Adarsh Das".to_owned(),
             value: 0.0,
+            current_window: Window::About,
         }
     }
 }
 
-impl TemplateApp {
+impl Website {
     /// Called once before the first frame.
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
         // This is also where you can customize the look and feel of egui using
@@ -35,7 +45,7 @@ impl TemplateApp {
     }
 }
 
-impl eframe::App for TemplateApp {
+impl eframe::App for Website {
     /// Called by the frame work to save state before shutdown.
     fn save(&mut self, storage: &mut dyn eframe::Storage) {
         eframe::set_value(storage, eframe::APP_KEY, self);
@@ -50,51 +60,59 @@ impl eframe::App for TemplateApp {
             // The top panel is often a good place for a menu bar:
 
             egui::menu::bar(ui, |ui| {
-                #[cfg(not(target_arch = "wasm32"))] // no File->Quit on web pages!
-                {
-                    ui.menu_button("File", |ui| {
-                        if ui.button("Quit").clicked() {
-                            _frame.close();
-                        }
-                    });
-                    ui.add_space(16.0);
-                }
-                ui.menu_button("File", |ui| {
-                    if ui.button("Quit").clicked() {
-                        // TODO
-                    }
-                });
-                ui.add_space(16.0);
+                egui::widgets::global_dark_light_mode_switch(ui);
 
-                egui::widgets::global_dark_light_mode_buttons(ui);
+                ui.separator();
+            
+                if ui.button("About").clicked() {
+                    self.current_window = Window::About;
+                }
+                if ui.button("Resume").clicked() {
+                    self.current_window = Window::Resume;
+                }
+                if ui.button("Projects").clicked() {
+                    self.current_window = Window::Projects;
+                }
+
+                ui.add_space(16.0);
             });
         });
 
         egui::CentralPanel::default().show(ctx, |ui| {
-            // The central panel the region left after adding TopPanel's and SidePanel's
-            ui.heading("eframe template");
+            match self.current_window {
+                Window::About => {
+                    ui.heading("About Me");
+                }
+                Window::Resume => {
+                    // The central panel the region left after adding TopPanel's and SidePanel's
+                    ui.heading("Resume");
 
-            ui.horizontal(|ui| {
-                ui.label("Go on write something");
-                ui.text_edit_singleline(&mut self.label);
-            });
+                    ui.horizontal(|ui| {
+                        ui.label("Go on write something");
+                        ui.text_edit_singleline(&mut self.label);
+                    });
 
-            ui.add(egui::Slider::new(&mut self.value, 0.0..=10.0).text("value"));
-            if ui.button("Increase number").clicked() {
-                self.value += 0.1_f32;
+                    ui.add(egui::Slider::new(&mut self.value, 0.0..=10.0).text("value"));
+                    if ui.button("Increase number").clicked() {
+                        self.value += 0.1_f32;
+                    }
+
+                    ui.separator();
+
+                    ui.add(egui::github_link_file!(
+                        "https://github.com/emilk/eframe_template/blob/master/",
+                        "Source code."
+                    ));
+
+                    ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
+                        powered_by_egui_and_eframe(ui);
+                        egui::warn_if_debug_build(ui);
+                    });
+                }
+                Window::Projects => {
+                    ui.heading("Projects");
+                }
             }
-
-            ui.separator();
-
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/master/",
-                "Source code."
-            ));
-
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                powered_by_egui_and_eframe(ui);
-                egui::warn_if_debug_build(ui);
-            });
         });
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ fn main() -> eframe::Result<()> {
     eframe::run_native(
         "eframe template",
         native_options,
-        Box::new(|cc| Box::new(eframe_template::TemplateApp::new(cc))),
+        Box::new(|cc| Box::new(adarsh_website::Website::new(cc))),
     )
 }
 
@@ -31,7 +31,7 @@ fn main() {
             .start(
                 "the_canvas_id", // hardcode it
                 web_options,
-                Box::new(|cc| Box::new(eframe_template::TemplateApp::new(cc))),
+                Box::new(|cc| Box::new(eframe_template::Website::new(cc))),
             )
             .await
             .expect("failed to start eframe");
