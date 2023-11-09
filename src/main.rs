0e0875a277662dd6fc3a5d3f51e273040f7a0514
Saphereye <adarshdas950@gmail.com>
2023-11-09 17:28:08 +0530

    fix: changed cargo version 2023 -> 2021

--- src/main.rs
+++ src/main.rs
@@ -31,7 +31,7 @@ fn main() {
             .start(
                 "the_canvas_id", // hardcode it
                 web_options,
-                Box::new(|cc| Box::new(eframe_template::Website::new(cc))),
+                Box::new(|cc| Box::new(adarsh_website::Website::new(cc))),
             )
             .await
             .expect("failed to start eframe");
