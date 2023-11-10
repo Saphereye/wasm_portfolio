c146c5fc1d28b7c8e8aafbbe70270b0af97d9ab9
Saphereye <adarshdas950@gmail.com>
2023-11-10 14:08:33 +0530

    feat: added basic graphing calculator skeleton

--- src/app.rs
+++ src/app.rs
@@ -1,8 +1,132 @@
-#[derive(serde::Deserialize, serde::Serialize)]
+use std::{f64::consts::TAU, fmt::format};
+use egui::*;
+
+
+use egui_plot::{
+    Arrows, AxisBools, AxisHints, Bar, BarChart, BoxElem, BoxPlot, BoxSpread, CoordinatesFormatter,
+    Corner, GridInput, GridMark, HLine, Legend, Line, LineStyle, MarkerShape, Plot, PlotImage,
+    PlotPoint, PlotPoints, PlotResponse, Points, Polygon, Text, VLine,
+};
+
+#[derive(serde::Deserialize, serde::Serialize, PartialEq)]
 enum Window {
     About,
     Resume,
     Projects,
+    GraphingCalculator,
+}
+
+#[derive(serde::Deserialize, serde::Serialize)]
+struct GraphingCalculatorApp {
+    circle_radius: f64,
+    circle_center: Pos2,
+    time: f64,
+    // line_style: LineStyle,
+    animate: bool,
+    show_axes: bool,
+    show_grid: bool,
+
+    square: bool,
+    proportional: bool,
+
+    coordinates: bool,
+
+    equations: Vec<String>,
+}
+
+impl Default for GraphingCalculatorApp {
+    fn default() -> Self {
+        GraphingCalculatorApp {
+            circle_center: Pos2 { x: 0.0, y: 0.0 },
+            circle_radius: 1.5,
+            time: 31.4,
+            // line_style: LineStyle::Solid,
+            animate: false,
+            show_axes: true,
+            show_grid: true,
+            square: false,
+            proportional: true,
+            coordinates: true,
+            equations: vec!["sin(x)".to_string(), "cos(x)".to_string()],
+        }
+        // Self { circle_radius: Default::default(), circle_center: Default::default(), time: Default::default(), line_style: Default::default(), animate: Default::default(), show_axes: Default::default(), show_grid: Default::default(), square: Default::default(), proportional: Default::default(), coordinates: Default::default(), equations: Default::default() }
+    }
+}
+
+impl GraphingCalculatorApp {
+    fn circle(&self) -> Line {
+        let n = 512;
+        let circle_points: PlotPoints = (0..=n)
+            .map(|i| {
+                let t = remap(i as f64, 0.0..=(n as f64), 0.0..=TAU);
+                let r = self.circle_radius;
+                [
+                    r * t.cos() + self.circle_center.x as f64,
+                    r * t.sin() + self.circle_center.y as f64,
+                ]
+            })
+            .collect();
+        Line::new(circle_points)
+            .color(Color32::from_rgb(100, 200, 100))
+            // .style(self.line_style)
+            .name("circle")
+    }
+
+    fn sin(&self) -> Line {
+        let time = self.time;
+        Line::new(PlotPoints::from_explicit_callback(
+            move |x| 0.5 * (2.0 * x).sin() * time.sin(),
+            ..,
+            512,
+        ))
+        .color(Color32::from_rgb(200, 100, 100))
+        // .style(self.line_style)
+        .name("sin")
+    }
+
+    fn thingy(&self) -> Line {
+        let time = self.time;
+        Line::new(PlotPoints::from_parametric_callback(
+            move |t| ((2.0 * t + time).sin(), (3.0 * t).sin()),
+            0.0..=TAU,
+            256,
+        ))
+        .color(Color32::from_rgb(100, 150, 250))
+        // .style(self.line_style)
+        .name("x = sin(2t), y = sin(3t)")
+    }
+
+    fn ui(&mut self, ui: &mut Ui) -> Response {
+        if self.animate {
+            ui.ctx().request_repaint();
+            self.time += ui.input(|i| i.unstable_dt).at_most(1.0 / 30.0) as f64;
+            // self.time += 0.1;
+        };
+        let mut plot = Plot::new("lines_demo")
+            .legend(Legend::default())
+            .y_axis_width(4)
+            .show_axes(self.show_axes)
+            .show_grid(self.show_grid);
+        if self.square {
+            plot = plot.view_aspect(1.0);
+        }
+        if self.proportional {
+            plot = plot.data_aspect(1.0);
+        }
+        if self.coordinates {
+            plot = plot.coordinates_formatter(Corner::LeftBottom, CoordinatesFormatter::default());
+        }
+        plot.show(ui, |plot_ui| {
+            plot_ui.line(self.circle());
+            plot_ui.line(self.sin());
+            plot_ui.line(self.thingy());
+        })
+        .response
+    }
+
+    fn parse_equation(&self) {
+        todo!();
+    }
 }
 
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
@@ -16,15 +140,17 @@ pub struct Website {
     value: f32,
 
     current_window: Window,
+
+    graphing_calculator_app: GraphingCalculatorApp,
 }
 
 impl Default for Website {
     fn default() -> Self {
         Self {
-            // Example stuff:
             label: "Adarsh Das".to_owned(),
             value: 0.0,
             current_window: Window::About,
+            graphing_calculator_app: GraphingCalculatorApp::default(),
         }
     }
 }
@@ -58,22 +184,18 @@ impl eframe::App for Website {
 
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
             // The top panel is often a good place for a menu bar:
-
             egui::menu::bar(ui, |ui| {
                 egui::widgets::global_dark_light_mode_switch(ui);
 
                 ui.separator();
-            
-                if ui.button("About").clicked() {
-                    self.current_window = Window::About;
-                }
-                if ui.button("Resume").clicked() {
-                    self.current_window = Window::Resume;
-                }
-                if ui.button("Projects").clicked() {
-                    self.current_window = Window::Projects;
-                }
-
+                ui.selectable_value(&mut self.current_window, Window::About, "👨 About");
+                ui.selectable_value(&mut self.current_window, Window::Resume, "📄 Resume");
+                ui.selectable_value(&mut self.current_window, Window::Projects, "🚀 Projects");
+                ui.selectable_value(
+                    &mut self.current_window,
+                    Window::GraphingCalculator,
+                    "📈 Graphing Calculator",
+                );
                 ui.add_space(16.0);
             });
         });
@@ -111,6 +233,30 @@ impl eframe::App for Website {
                 }
                 Window::Projects => {
                     ui.heading("Projects");
+                }
+                Window::GraphingCalculator => {
+                    egui::SidePanel::right("graph_panel")
+                        .default_width(50.0)
+                        .show(ctx, |ui| {
+                            self.graphing_calculator_app.ui(ui);
+                        });
+
+                    egui::CentralPanel::default().show(ctx, |ui| {
+                        // self.graphing_calculator_app.equations.push("69420".to_string());
+                        ui.heading("Equations");
+                        // ui.horizontal(|ui| {
+                        //     ui.text_edit_singleline(&mut self.graphing_calculator_app.equations[0]);
+                        //     // ui.text_edit_singleline(&mut self.graphing_calculator_app.equations[1]);
+                        // });
+                        ui.vertical(|ui| {
+                            for equation in self.graphing_calculator_app.equations.iter_mut() {
+                                ui.text_edit_singleline(equation);
+                            }
+                         });
+                        ui.heading(format!("{:?}", self.graphing_calculator_app.equations));
+                        ui.ctx().request_repaint();
+                        // ui.heading(self.graphing_calculator_app.equations[1].clone());
+                    });
                 }
             }
         });
